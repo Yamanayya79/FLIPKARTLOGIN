e9bc7668a92cc973d5b01a9cd2de18e25f9b61c8
Yamanayya79 <108881704+Yamanayya79@users.noreply.github.com>
2023-03-11 01:06:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const db = require('./db');
-const port = 5002;
+const port = process.env.PORT || 5002;
 const cors = require('cors');
 
 app.use(cors());
